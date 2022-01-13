9bbed6c806535fe66cbf27644c1f1a0c41e087b5
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-13 16:14:47 +0100

    bonus - JSnack 4 completed

--- js/snack4.js
+++ js/snack4.js
@@ -1,5 +1,5 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
-
+const result = document.getElementById('result');
 const numbers = [1, 5, 7, 4, 9, 3, 25, 30, 51, 22];
 let i = 0;
 let sum = 0;
@@ -11,3 +11,4 @@ while (i < numbers.length) {
     i++;
 }
 console.log(sum);
+result.innerHTML = sum;
