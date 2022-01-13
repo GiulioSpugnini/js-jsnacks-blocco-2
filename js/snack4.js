09f112b1578a1fd10f86bc816085dfaedb13b548
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-13 15:33:13 +0100

    JSnack4 solution

---
+++ js/snack4.js
@@ -0,0 +1,13 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const numbers = [1, 5, 7, 4, 9, 3, 25, 30, 51, 22];
+let i = 0;
+let sum = 0;
+while (i < numbers.length) {
+
+    if (i % 2 != 0) {
+        sum += numbers[i];
+    }
+    i++;
+}
+console.log(sum);
