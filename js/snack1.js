d821c83b0464c6eabb9ce19c0c40fde7c270a92c
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-13 19:00:25 +0100

    superbonus- JSnack1 completed

--- js/snack1.js
+++ js/snack1.js
@@ -1,16 +1,36 @@
 //Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. 
 //Esegui questo programma in due versioni, con il for e con il while.
 const result = document.getElementById('result');
+const enter = document.getElementById('enter');
+const inputText = document.getElementById('inputText');
+const add = document.getElementById('add');
+// const inputUser = document.createElement('input');
+// inputUser.setAttribute("input", "text");
 let sum = 0;
-let i = 0;
-//#WHILE
-while (i !== 5) {
-    const userNumber = parseInt(prompt('inserisci un numero'));
-    sum += userNumber;
-    i++;
-}
+result.classList.add('d-none');
+const numbers = [];
+add.addEventListener('click', function() {
+    let valid = false;
+    //#WHILE
+    while (valid == false) {
+        numbers.push(inputText.value);
+        sum += parseInt(inputText.value);
+        console.table(numbers);
+
+        valid = true;
+    }
+    if (numbers.length == 5) {
+        add.classList.add('d-none');
+    }
+})
+console.log(numbers);
 console.log(sum);
-result.innerHTML = sum;
+
+enter.addEventListener('click', function() {
+    result.classList.remove('d-none');
+    result.innerHTML = `${numbers[0]} + ${numbers[1]} + ${numbers[2]} + ${numbers[3]} + ${numbers[4]} La loro somma è : ${sum}`;
+
+})
 
 
 
