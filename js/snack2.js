5fd6dd3ca9f140fcb5175a346b81e9c97511c827
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-13 19:20:40 +0100

    super bonus - JSnack 2 completed LAST COMMIT

--- js/snack2.js
+++ js/snack2.js
@@ -1,23 +1,36 @@
 //Inserisci un numero, se è pari stampa il numero,se è dispari stampa il numero successivo
 
 const result = document.getElementById('result');
-
+const enter = document.getElementById('enter');
+const inputText = document.getElementById('inputText');
+const add = document.getElementById('add');
+let resultName = '';
 let solution = 0;
-const userNumber = parseInt(prompt('Inserisci un numero'));
 let i = 0;
+
+add.addEventListener('click', function() {
+    while (i < 1) {
+        if (inputText.value % 2 == 0) {
+            // console.log(inputText.value);
+            solution = parseInt(inputText.value);
+            resultName = 'è pari';
+        } else {
+            console.log(inputText.value + 1);
+            solution = parseInt(inputText.value) + 1;
+            resultName = 'è dispari';
+            // console.log(inputText.value);
+        }
+        i++;
+
+    }
+    add.classList.add('d-none');
+})
+
 //!VALIDATION
-if (isNaN(userNumber)) {
+if (isNaN(inputText.value)) {
     alert('Puoi inserire solo un numero');
 }
-while (i < 1) {
-    if (userNumber % 2 == 0) {
-        console.log(userNumber);
-        solution = userNumber;
-    } else {
-        console.log(userNumber + 1);
-        solution = userNumber + 1;
-    }
-    i++;
-
-}
-result.innerHTML = solution;
+enter.addEventListener('click', function() {
+    result.innerHTML = `Il numero: ${inputText.value} ${resultName} quindi esce ${solution}`;
+    enter.classList.add('d-none');
+})
