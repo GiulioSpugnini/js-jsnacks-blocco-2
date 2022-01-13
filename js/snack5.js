0e4bf7108ed0454b31b03d09e9bfc5b1d0f0943e
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-13 16:17:40 +0100

    bonus - JSnack 5 completed

--- js/snack5.js
+++ js/snack5.js
@@ -1,6 +1,6 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi casuali all’array che ha meno elementi,fino a quando ne avrà tanti quanti l’altro.
-
+const result = document.getElementById('result');
 let first = [2, 3, 1];
 let second = [12, 5, 8, 9];
 
@@ -9,3 +9,4 @@ while (first.length < second.length) {
     first.push(addName);
 }
 console.log(first, second);
+result.innerHTML = ` Primo array: ${first} Secondo array:${second} `;
